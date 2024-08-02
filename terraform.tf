2089c8a2fa792e798f86974039897814c381331c
Heena 1. Verma (EXT-Nokia) <vheena176@gmail.com>
2024-08-03 00:52:16 +0530

    new rg2

--- terraform.tf
+++ terraform.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.114.0"
-    }
-  }
-}
 
-provider "azurerm" {
-  features{}
-}
 
 resource "azurerm_resource_group" "rg1" {
   name     = "rg1"
